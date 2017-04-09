8ccaa16b524747000f24addc7ebcd810b6899c51
Enric Serra <enricserrasanz@gmail.com>
2017-04-09 10:22:53 +0100

    javadocs

--- app/src/main/java/com/bignerdranch/android/IamWatchingYou/Position.java
+++ app/src/main/java/com/bignerdranch/android/IamWatchingYou/Position.java
@@ -1,29 +1,61 @@
 package com.bignerdranch.android.IamWatchingYou;
 
-
+/**
+ * A class used to handle any position.
+ */
 
 public class Position {
 
+    /**
+     * The latitude of the position in radians.
+     */
     private double latitude;
+
+    /**
+     * The longitude of the position in radians.
+     */
     private double longitude;
+
+    /**
+     * A String describing the position.
+     */
     private String description;
 
+    /**
+     * Constructor
+     * @param latitude the latitude in degrees.
+     * @param longitude the longitude in degrees.
+     * @param description a string describing the position
+     */
     public Position(double latitude,
-                    double longitude, String description) {
+                    double longitude,
+                    String description) {
         this.latitude = Math.toRadians(latitude);
         this.longitude = Math.toRadians(longitude);
         this.description = description;
     }
 
-    //Created for saving separately in our database
+
+    /**
+     * Getter for latitude.
+     * @return the latitude in radians.
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Getter for longitude
+     * @return the longitude in radians
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * Getter for the description.
+     * @return the description.
+     */
     public String getDescription() {
         return description;
     }
